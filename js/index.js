1b315526eb54e033c10b13c930cf55acb7273a8b
Ho Van Phong <phongckm93@gmail.com>
2022-07-16 01:37:30 +0700

    fix bug

--- js/index.js
+++ js/index.js
@@ -10,12 +10,11 @@ Ngược lại, khi click vào nút sun ☀️, ta sẽ xóa class light để c
 //gọi thẻ button có id là switchButton
 // gắn chức năng click cho switchButton
 document.getElementById("switchButton").onclick = function () {
-    //khi button được click thì tìm đến thẻ body có id là myBody
-    // dùng classlist để gọi thuộc tính class của thẻ body
-    //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class light thì toggle sẽ thêm class light vào body. Ngược lại, nếu thẻ body đang có class light thì toogle sẽ xóa class light đi.
-    document.getElementById("myBody").classList.toggle("light");
-  };
-  
+  //khi button được click thì tìm đến thẻ body có id là myBody
+  // dùng classlist để gọi thuộc tính class của thẻ body
+  //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class light thì toggle sẽ thêm class light vào body. Ngược lại, nếu thẻ body đang có class light thì toogle sẽ xóa class light đi.
+  document.getElementById("myBody").classList.toggle("light");
+};
 
 
 
@@ -43,75 +42,75 @@ document.getElementById("switchButton").onclick = function () {
 
 
 
-  
+
+
 //Swipe carousel
 
-  $( document ).ready(function() {
+$(document).ready(function () {
 
-    $(".carousel").carousel({
-        interval: true,
-        pause: false,
-    });
-  
-    $( ".carousel .carousel-inner" ).swipe( {
-    swipeLeft: function ( event, direction, distance, duration, fingerCount ) {
-        this.parent( ).carousel( 'next' );
+  $(".carousel").carousel({
+    interval: true,
+    pause: true,
+  });
+
+  $(".carousel .carousel-inner").swipe({
+    swipeLeft: function (event, direction, distance, duration, fingerCount) {
+      this.parent().carousel('next');
     },
-    swipeRight: function ( ) {
-        this.parent( ).carousel( 'prev' );
+    swipeRight: function () {
+      this.parent().carousel('prev');
     },
     threshold: 0,
-    tap: function(event, target) {
-        window.location = $(this).find('.carousel-item.active a').attr('href');
+    tap: function (event, target) {
+      window.location = $(this).find('.carousel-item.active a').attr('href');
     },
-    excludedElements:"label, button, input, select, textarea, .noSwipe"
-    } );
-  
-    $('.carousel .carousel-inner').on('dragstart', 'a', function () {
-        return false;
-    });  
-  
+    excludedElements: "label, button, input, select, textarea, .noSwipe"
   });
+
+  $('.carousel .carousel-inner').on('dragstart', 'a', function () {
+    return false;
+  });
+
+});
 
 //open menu and close menu
 
-  var navBar = document.getElementById("navbarSupportedContent");
-  var openBar = document.getElementById("open");
-  var closeBar = document.getElementById("closer");
-  function windowResize(breakpoint) {
+var navBar = document.getElementById("navbarSupportedContent");
+var openBar = document.getElementById("open");
+var closeBar = document.getElementById("closer");
+function windowResize(breakpoint) {
 
-    // If media query matches
-    if (breakpoint.matches) {
-      navBar.classList.add("changeBar");
-      openBar.classList.remove("none");
-      closeBar.classList.add("none");
-    } else {
-      navBar.classList.remove("changeBar");
-      openBar.classList.add("none");
-      closeBar.classList.add("none");
-    }
-  }
-  var breakpoint = window.matchMedia("(max-width: 1200px)")
-  // Call listener function at run time
-  windowResize(breakpoint);
-  // Attach listener function on state changes
-  breakpoint.addEventListener("change", () => {
-    console.log(breakpoint)
-    windowResize(breakpoint);
-
-  });
-
-  openBar.addEventListener("click", function () {
-    navBar.classList.remove("changeBar");
-    closeBar.classList.remove("none");
-    openBar.classList.add("none");
-  });
-  closeBar.addEventListener("click", function () {
+  // If media query matches
+  if (breakpoint.matches) {
     navBar.classList.add("changeBar");
     openBar.classList.remove("none");
     closeBar.classList.add("none");
-  });
+  } else {
+    navBar.classList.remove("changeBar");
+    openBar.classList.add("none");
+    closeBar.classList.add("none");
+  }
+}
+var breakpoint = window.matchMedia("(max-width: 1200px)")
+// Call listener function at run time
+windowResize(breakpoint);
+// Attach listener function on state changes
+breakpoint.addEventListener("change", () => {
+  console.log(breakpoint)
+  windowResize(breakpoint);
+
+});
+
+openBar.addEventListener("click", function () {
+  navBar.classList.remove("changeBar");
+  closeBar.classList.remove("none");
+  openBar.classList.add("none");
+});
+closeBar.addEventListener("click", function () {
+  navBar.classList.add("changeBar");
+  openBar.classList.remove("none");
+  closeBar.classList.add("none");
+});
 
 
 
-  
